ead7119e416e4985180d997644c49f348e34d5df
Shivam Mishra <shivaymishraeee@gmail.com>
2024-08-29 13:42:22 +0530

    Set the routes for home and post

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,43 @@
+import styled, { ThemeProvider } from "styled-components";
+import {darkTheme} from "./utils/Theme";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+const Container = styled.div`
+  width:100%;
+  height:100%;
+  display:flex;
+  background:${({theme}) => theme.bg};
+  color : ${({theme}) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 3;
+`;
 
 function App() {
   return (
-    <>
-    <div>Hello World</div>
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <Container>
+        <Wrapper>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home/>} exact/>
+              <Route path="/post" element={<CreatePost/>} exact/>
+            </Routes>
+          </BrowserRouter>
+        </Wrapper>
+      </Container>
+    </ThemeProvider>
   );
 }
 
